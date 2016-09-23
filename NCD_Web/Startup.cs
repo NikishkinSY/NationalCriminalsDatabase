57f3af108c3e498e416d8499dfc34fad98546291
Nikishkinsy <nikishkinsy@gmail.com>
2016-09-23 16:41:44 +0300

    - create architecture

---
+++ NCD_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCD_Web.Startup))]
+namespace NCD_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
